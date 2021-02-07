4eb64ecbe1c8feac86136dc971a0d0138221b305
Stéphane PEILLET <stephane.peillet@gmail.com>
2021-02-07 13:03:20 +0100

    Add korasaju files

--- Course 2 - Graph Search /Codes/DFS.py
+++ Course 2 - Graph Search /Codes/DFS.py
@@ -1,20 +1,58 @@
-def DFS_intern(graph:dict, start_vertex:int, explored:list=[])->list:
+from tqdm import tqdm
+
+class Params:
+    '''
+        Parameters passed during graph exploration
+    '''
+    def __init__(self):
+        self.explored = []
+        self.s = 0
+        self.leader = {}
+        self.leaders = []
+        self.t = 0
+        self.finishing_time = {}
+        self.SCCs = {}
+
+def DFS_simple_exploration(graph:dict, start_vertex:int, params:Params=None)->list:
+    '''
+        Apply recursively DFS from a starting node to explored all linked nodes.
+    '''
+    if params == None:
+        params = Params()
+    params.explored.append(start_vertex)
+    for node in graph[start_vertex]:
+        if node not in params.explored:
+            params = DFS_simple_exploration(graph, node, params=params)
+    return params   
+        
+        
+def DFS(graph:dict, node_i:int, params:Params=None)->Params:
     '''
         Intern loop to apply recursively DFS from a node to one of his not explored linked node.
     '''
-    explored.append(start_vertex)
-    for node in graph[start_vertex]:
-        if node not in explored:
-            DFS_intern(graph, node, explored=explored)
+    if params == None:
+        params = Params()
+    params.explored.append(node_i)
+    params.leader[node_i]= params.s
+    params.SCCs[params.s].append(node_i) 
+    for node_j in graph[node_i]:
+        if node_j not in params.explored:
+            params = DFS(graph, node_j, params=params)
+    params.t += 1
+    params.finishing_time[node_i]= params.t
+    return params 
 
-def DFS(graph:dict)-> list:
+
+def DFS_loop(graph:dict)-> list:
     '''
         Apply DFS on a graph and returned an exploring list.
         Here we apply DFS to every node not explored in the graph.
     '''
     nodes = list(graph.keys())
-    explored = [nodes[0]]
-    for node in nodes:
-        if node not in explored:
-            DFS_intern(graph, node, explored=explored)
-    return explored
+    params = Params()
+    for node in tqdm(reversed(nodes)):
+        if node not in params.explored:
+            params.s = node
+            params.SCCs[params.s] = []
+            params = DFS(graph, node, params=params)
+    return params 
---
+++ Course 2 - Graph Search /Codes/korasaju.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+import os, sys, threading
+from DFS import *
+
+def sorted_dict(input_dict:dict)->dict:
+    '''
+        Sorted a dict by key values
+    '''
+    output_dict = {}
+    for key in sorted(input_dict):
+        output_dict[key] = input_dict[key]
+    return output_dict
+
+
+def get_reverse_graph(graph:dict)->dict:
+    '''
+        Return a graph with reversed links
+    '''
+    graph_reverse = {}
+    for key in graph.keys():
+        for value in graph[key]:
+            if value == []:
+                graph_reverse[key]=[]
+            else:
+                if value in graph_reverse.keys():
+                    graph_reverse[value].append(key)
+                else:
+                    graph_reverse[value] = [key]
+    return sorted_dict(graph_reverse)
+
+
+def get_finishing_time_graph(graph:dict, finishing_time:dict)->dict:
+    '''
+        Return a new graph by replacing graph's node by their finishing time.
+    '''
+    ft_graph = {}
+    for key in graph.keys():
+        values = []
+        for value in graph[key]:
+            values.append(finishing_time[value])
+        ft_graph[finishing_time[key]] = values
+    return sorted_dict(ft_graph)
+
+
+def korasaju(G:dict, Grev:dict=None, verbose:bool=False)->(list, dict):
+    '''
+        Return SCCs of a graph with Korasaju's two passes algorithm.
+    '''
+    if Grev is None:
+        Grev = get_reverse_graph(G)
+    Grev_params = DFS_loop(Grev) # Run DFS_loop on reverse graph to compute the "magical ordering" of nodes
+    G_finishing_time = get_finishing_time_graph(G, Grev_params.finishing_time)
+    Gft_params = DFS_loop(G_finishing_time)
+    Gft_params.leaders = set(Gft_params.leader.values())
+    
+    if verbose:
+        print('---------Graphs---------')
+        print('Input  :', G)
+        print('Step 1 :', Grev)
+        print('Step 2 :', sorted_dict(Grev_params.finishing_time))
+        print('Step 3 :', G_finishing_time)
+        print('---------Results--------')
+        print('Leader : ', Gft_params.leader)
+        print('Leaders : ', Gft_params.leaders)
+        print('SCCs :', Gft_params.SCCs)
+    else:
+        return Gft_params.leaders, Gft_params.SCCs
+
+
+def load_data(filepath:str)->(dict, dict):
+    '''
+        Create graph and his reverse as dict from the input file
+    '''
+    file = open(filepath,"r")
+    data = file.readlines()
+    
+    # Determine all nodes present in the graph
+    nodes = set()
+    for line in data:
+        res = list(map(lambda x: int(x),line.split()))
+        nodes.update(res)
+    
+    # Init all the nodes in the graph
+    graph, graph_rev = {node:[] for node in nodes}, {node:[] for node in nodes}
+    
+    for line in data:
+        node_start, node_target = list(map(lambda x: int(x),line.split())) 
+        # Graph 
+        if node_start not in graph.keys():
+            graph[node_start] = [node_target]
+        else:
+            graph[node_start].append(node_target)
+        # Reverese graph
+        if node_target not in graph_rev.keys():
+            graph_rev[node_target] = [node_start]
+        else:
+            graph_rev[node_target].append(node_start)
+            
+    return graph, graph_rev
+
+
+def main(argv):
+    filepath = str(argv)
+    if os.path.isfile(filepath):
+        graph, graph_rev = load_data(filepath)
+        threading.stack_size(67108864)
+        sys.setrecursionlimit(2 ** 20)  
+        results = korasaju(graph, graph_rev)
+        return results
+    
+    else:
+        print('File not found ...')
+        sys.exit()
+    
+if __name__ == '__main__':
+    main(sys.argv[1])
