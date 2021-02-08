d369304bd1b2410ca521be8cb4fdbffbcd49132d
Stéphane PEILLET <stephane.peillet@gmail.com>
2021-02-08 21:15:04 +0100

    Changes on notebooks

--- Course 2 - Graph Search /Codes/korasaju.py
+++ Course 2 - Graph Search /Codes/korasaju.py
@@ -55,16 +55,18 @@ def korasaju(G:dict, Grev:dict=None, use_stack:bool=False,verbose:bool=False)->P
     Gft_params.leaders = set(Gft_params.leader.values())
     
     if verbose:
-        print('---------Graphs---------')
-        print('Input  :', G)
-        print('Step 1 :', Grev)
-        print('Step 2 :', sorted_dict(Grev_params.finishing_time))
-        print('Step 3 :', G_finishing_time)
+        print('---------Graphs---------') 
+        print('Input  :                                ', G)
+        print('Step 1 - Reverse graph :                ', Grev)
+        print('Step 2 - Compute finishing time:        ', sorted_dict(Grev_params.finishing_time))
+        print('Step 3 - Find the leaders for each node:', G_finishing_time)
         print('---------Results--------')
         print('Leader : ', Gft_params.leader)
         print('Leaders : ', Gft_params.leaders)
         print('SCCs :', Gft_params.SCCs)
+        
         return Gft_params
+    
     else:
         return Gft_params
 
