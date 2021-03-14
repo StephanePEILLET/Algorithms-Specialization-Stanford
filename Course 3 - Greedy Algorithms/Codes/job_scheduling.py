05236b7da82a55f41ee20b5e3b1ed8977035da5f
Stéphane PEILLET <stephane.peillet@gmail.com>
2021-03-14 15:17:31 +0100

    Add Jobs Scheduling and Prims

---
+++ Course 3 - Greedy Algorithms/Codes/job_scheduling.py
@@ -0,0 +1,39 @@
+def load_data(filepath:str)->list:
+    '''
+        Load the list of processes and return their number, their weigth and their length.
+    '''
+    import pandas as pd
+    file = open(filepath, 'r')
+    lines = file.readlines()
+    n_jobs = int(lines[0].strip())
+    jobs = [tuple(map(int, x.strip().split(' '))) for x in lines[1:]]
+    df_jobs = pd.DataFrame(jobs, columns=['weigth', 'length'])
+    assert(len(df_jobs) == n_jobs)
+    return df_jobs
+
+def compute_cost(df:pd.DataFrame)->int:
+    '''
+        Return as cost the sum of the product weigth with length. 
+    '''
+    cost = 0
+    for idx, job in df.iterrows():
+        cost += job['weigth'] * df['length'][idx:].sum()
+    return cost
+
+def compute_order(df:pd.DataFrame)->(list, list):
+    '''
+        Compute process order with difference and ratio between their weigth and their length.
+    '''
+    order_diff = (df['weigth'] - df['length']).sort_values().index.values.astype(int)
+    order_ratio = (df['weigth'] / df['length']).sort_values().index.values.astype(int)
+    return order_diff, order_ratio
+
+def compute_costs(df:pd.DataFrame)->(int, int, int):
+    '''
+        Compute costs with several order: unordered, difference and ratio.
+    '''
+    order_diff, order_ratio = compute_order(df)
+    unorder_cost = compute_cost(df)
+    diff_cost = compute_cost(df.loc[order_diff])
+    ratio_cost = compute_cost(df.loc[order_ratio])
+    return unorder_cost, diff_cost, ratio_cost
---
+++ Course 3 - Greedy Algorithms/Codes/prims.py
@@ -0,0 +1,54 @@
+def load_data(filepath:str)->(dict, int, int):
+    '''
+        Load graph from edges.txt
+    '''
+    graph = {}
+    file = open(filepath, 'r')
+    lines = [list(map(int, line.strip().split(' '))) for line in file.readlines()]
+    
+    n_nodes, n_edges = lines.pop(0)
+    
+    for line in lines:
+        node1, node2, cost = line
+        
+        # Define edge from node1 to node2
+        if node1 in graph.keys():
+            graph[node1][node2] = cost
+        else:
+            graph[node1] = {node2 : cost}
+            
+        # Then also define edge from node2 to node1
+        if node2 in graph.keys():
+            graph[node2][node1] = cost
+        else:
+            graph[node2] = {node1 : cost}
+            
+    return graph, n_nodes, n_edges 
+
+def prims(graph:dict, start_vertex:int=None)->(list, int):
+    '''
+        Prim's Algorithms straigthforward implementation for finding MST in a graph.
+    '''
+    import random
+    from heapq import heappush, heappop
+    
+    if not start_vertex:
+        start_vertex = random.choice(list(graph.keys()))
+    costs, T = [], []
+    X = [start_vertex] ; V = list(graph.keys()) # All nodes 
+    
+    while set(V) - set(X):
+        heap = []
+        for node in X:
+            for edge in graph[node]:
+                if edge not in X:
+                    heappush(heap, (graph[node][edge], node, edge)) 
+        if heap:  
+            cost, node1, node2 = heappop(heap)
+            X.append(node2)
+            T.append((node1, node2))
+            costs.append(cost)
+        else:
+            break
+            
+    return T, sum(costs)
